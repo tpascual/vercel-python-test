7ee429ee6e8ef254e3c6a29c9e6bdd28deb1faa1
tpascual <toni.pasqual@gmail.com>
2024-07-04 18:53:36 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,15 @@
-print ("hola")
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        # Obtener la fecha de inserción usando la función local
+        
+        # Usar una f-string para incluir el valor de la variable
+        response_text = f'Hello, world! {insertion_date}'
+        self.wfile.write(response_text.encode('utf-8'))
+        return

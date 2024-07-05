615c9330f029d61e78aad55e076ac27f5aade626
tpascual <toni.pasqual@gmail.com>
2024-07-05 10:00:32 +0200

    test db

--- api/index.py
+++ api/index.py
@@ -12,29 +12,43 @@ if uri is None:
 # Conectarse al cliente de MongoDB
 client = MongoClient(uri)
 
+def check_connection():
+    try:
+        # Intenta listar las bases de datos para verificar la conexión
+        client.list_database_names()
+        return True
+    except Exception as e:
+        print(f"Error al conectar con MongoDB: {e}")
+        return False
+
 # Seleccionar la base de datos y la colección
-db = client.testDante
-collection = db.helloCollection
+#db = client.testDante
+#collection = db.helloCollection
 
 # Crear el documento a insertar
-document = {
-    "message": "Hello"
-}
+#document = {
+#    "message": "Hello"
+#}
 
 # Insertar el documento en la colección
-result = collection.insert_one(document)
+#result = collection.insert_one(document)
 
 # Confirmar que el documento ha sido insertado
-print(f"Inserted document with id: {result.inserted_id}")
+#print(f"Inserted document with id: {result.inserted_id}")
 
 tata = os.environ.get('PPP')
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        text = f'hola {tata}'
-        self.wfile.write(text.encode('utf-8'))
+        if check_connection():
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('OK'.encode('utf-8'))
+        else:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('Failed to connect to MongoDB'.encode('utf-8'))
         return

8477db77ed74909023bd0b35a3b622039b3a5148
tpascual <toni.pasqual@gmail.com>
2024-07-05 10:11:57 +0200

    test

--- api/index.py
+++ api/index.py
@@ -2,38 +2,40 @@ from http.server import BaseHTTPRequestHandler
 import os
 from pymongo import MongoClient
 
-# Obtener la cadena de conexión desde la variable de entorno
-#uri = os.getenv("MONGODB_URI")
-uri = os.environ.get('MONGODB_URI')
-
-if uri is None:
-    raise ValueError("No se encontró la variable de entorno MONGODB_URI")
-
-# Conectarse al cliente de MongoDB
-client = MongoClient(uri)
-
-# Seleccionar la base de datos y la colección
-#db = client.testDante
-#collection = db.helloCollection
-
-# Crear el documento a insertar
-#document = {
-#    "message": "Hello"
-#}
-
-# Insertar el documento en la colección
-#result = collection.insert_one(document)
-
-# Confirmar que el documento ha sido insertado
-#print(f"Inserted document with id: {result.inserted_id}")
-
-tata = os.environ.get('PPP')
- 
 class handler(BaseHTTPRequestHandler):
- 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('OK'.encode('utf-8'))
+        # Obtener la cadena de conexión desde la variable de entorno
+        uri = os.environ.get('MONGODB_URI')
 
+        if uri is None:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('Variable de entorno MONGODB_URI no encontrada'.encode('utf-8'))
+            return
+
+        try:
+            # Conectarse al cliente de MongoDB
+            client = MongoClient(uri)
+            # Verificar la conexión listando las bases de datos
+            client.list_database_names()
+            # Si la conexión es exitosa, responder con "OK"
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('OK'.encode('utf-8'))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(f'Error al conectar con MongoDB: {str(e)}'.encode('utf-8'))
+
+# Inicializar y correr el servidor HTTP
+def run(server_class=BaseHTTPRequestHandler.HTTPServer, handler_class=handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
